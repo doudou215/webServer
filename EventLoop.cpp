f656756e09d79a2f437c1cb0225d7333b054c323
doudou215 <dream@20151223@13.com>
2021-01-19 19:57:05 +0800

    EventLoop part done

--- EventLoop.cpp
+++ EventLoop.cpp
@@ -1,16 +1,35 @@
 #include "EventLoop.h"
+#include <sys/epoll.h>
+#include <sys/eventfd.h>
+#include <iostream>
+#include "Util.h"
+#include "base/Logging.h"
 
 __thread EventLoop *t_loopInThisThread = 0;
 
+int createEventFd() {
+    int efd = eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
+    if (efd < 0) {
+        LOG << "failed in eventFd";
+        abort();
+    } 
+    return efd;
+}
+
 EventLoop::EventLoop():
     looping_(false),
-    threadId_(Current::tid()) {
-        if (t_loopInThisThread) {
-
-        }
-        else {
-            t_loopInThisThread = this;
-        }
+    threadId_(Current::tid()),
+    poller_(new Epoll(this)),
+    wakeupFd_(createEventFd()),
+    wakeupChannel_(new Channel(this, wakeupFd_)),
+    callingPendingFunctors_(false),
+    quit_(false),
+    eventHandling_(false) {
+        t_loopInThisThread = this;
+        wakeupChannel_->setEvents(EPOLLIN | EPOLLET);
+        wakeupChannel_->setReadHandler(std::bind(&EventLoop::handleRead(this)));
+        wakeupChannel_->setConnHanler(std::bind(&EventLoop::hanleConn, this));
+        poller_->epoll_add(wakeupChannel_, 0);
     }
 
 EventLoop::~EventLoop() {
--- EventLoop.h
+++ EventLoop.h
@@ -3,12 +3,13 @@
 #include <memory>
 #include <vector>
 #include "Channel.h"
-#include "Epoll.h"
-#include "Util.h"
-#include "base/CurrentThread.h"
-#include "base/Logging.h"
-#include "base/Thread.h"
+//#include "Epoll.h"
+//#include "Util.h"
+//#include "base/CurrentThread.h"
+//#include "base/Logging.h"
+//#include "base/Thread.h"
 #include "base/noncopyable.h"
+#include "base/Mutex.h"
 #include <iostream>
 
 class Epoll;
@@ -18,12 +19,26 @@ class EventLoop : noncopyable {
     typedef std::function<void()> Functors;
     EventLoop();
     ~EventLoop();
-    void assertInLoopThread() { 
-        assert(isInLoopTrehad());
-    }
-    void loop();
+    void assertInLoopThread() { assert(isInLoopTrehad()); }
     bool isInLoopTrehad() { return threadId_ == CurrentTread::tid(); }
+    void loop();
+    void quit();
+    void runInLoop(Functors &&cb);
+    void queueInLoop(Functors &&cb);
+    // what's this up to?
+    void shutdown(shared_ptr<Channel> channel) { shutDownWR(channel->getFd()); }
 
+    void removeFromPoller(SPChannel channel) {
+        poller_->epoll_del(channel);
+    }
+
+    void updatePoller(SPChannel channel, int timeout = 0) {
+        poller_->epoll_mod(channel, timeout);
+    }
+
+    void addPoller(SPChannel channel, int timeout = 0) {
+        poller_->epoll_add(channel, timeout);
+    }
  private:
     
     std::vector<Functors> pendingFunctors;
@@ -31,11 +46,14 @@ class EventLoop : noncopyable {
     bool looping_;
     bool quit_;
     bool eventHandling_;
-    bool callingPendingFunctors;
+    bool callingPendingFunctors_;
     mutable MutexLock mutex_;
     std::shared_ptr<Epoll> poller_;
     int wakeupFd_;
     SPChannel wakeupChannel_;
-
     
+    void wakeup();
+    void handleRead();
+    void hanleConn();
+    void doPendingFunctors();
 };
