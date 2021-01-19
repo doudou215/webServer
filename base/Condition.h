7f3c4bb5223bd7fa3b282dfea4c6a5f9e8537d94
doudou215 <dream@20151223@13.com>
2021-01-19 16:28:19 +0800

    complete Mutex

---
+++ base/Condition.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <errno.h>
+#include <pthread.h>
+#include <pthread.h>
+#include <time.h>
+#include <cstdint>
+#include "Mutex.h"
+#include "noncopyable.h"
+
+class Condition : noncopyable {
+
+};
---
+++ base/Mutex.h
@@ -0,0 +1,30 @@
+#include <pthread.h>
+#include "noncopyable.h"
+
+class MutexLock : noncopyable {
+ public:
+    MutexLock() {
+        pthread_mutex_init(&mutex_, NULL);
+    }
+
+    ~MutexLock() {
+        pthread_mutex_destroy(&mutex_);
+    }
+
+    pthread_mutex_t *get() { return &mutex_; }
+
+    void lock() { pthread_mutex_lock(&mutex_); }
+    void unlock() { pthread_mutex_unlock(&mutex_); }
+ private:
+    pthread_mutex_t mutex_;
+    friend class Contidion;
+};
+
+class MutexLockGuard : noncopyable {
+ public:
+    explicit MutexLockGuard(MutexLock &mutex) : mutex_(mutex) { mutex_.lock(); }
+    ~MutexLockGuard() { mutex_.unlock(); }
+
+ private:
+    MutexLock &mutex_;
+};
