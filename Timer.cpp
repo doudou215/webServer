218ae20eaf8e3babda823eaf55a9d1b5fb331ac2
doudou215 <dream@20151223@13.com>
2021-01-23 18:29:34 +0800

    finish the timer

---
+++ Timer.cpp
@@ -0,0 +1,66 @@
+#include "Timer.h"
+#include <sys/time.h>
+#include <unistd.h>
+#include <queue>
+
+// gettimeofday gives the number of seconds and microseconds since the Epoch
+// and aslo the timeout means milliseconds, what a load of crap
+TimerNode::TimerNode(SPHttpData data, int timeout): delete_(false), httpData_(data) {
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    expiredTime_ = (now.tv_sec % 1000) * 1000 + now.tv_usec / 1000 + timeout;
+
+}
+
+TimerNode::~TimerNode() {
+    if (httpData_)
+        httpData_->handleClose();
+}
+
+// ct do not set delete_ attribute?
+TimerNode::TimerNode(TimerNode &tn) : expiredTime_(0), httpData_(tn.httpData_) {
+
+}
+
+void TimerNode::update(int timeout) {
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    expiredTime_ = (now.tv_sec % 1000) * 1000 + now.tv_usec / 1000 + timeout;
+}
+
+bool TimerNode::isValid() {
+    size_t expiredTime;
+    struct timeval now;
+    gettimeofday(&now, NULL);
+    expiredTime = (now.tv_sec % 1000) * 1000 + now.tv_usec / 1000;
+    if (expiredTime > expiredTime_) {
+        setDelete();
+        //httpData_->reset();
+        return false;
+    }
+    return true;
+}
+
+void TimerNode::clearReq() {
+    if (httpData_)
+        httpData_.reset();
+    setDelete();
+}
+
+void TimerManager::add_timer(SPHttpData data, int timeout) {
+    SPTimerNode node(new TimerNode(data, timeout));
+    TimerQueue_.push(node);
+    SPHttpData->linkTimer(node);  // not clear with this function?
+}
+
+void TimerManager::handleExpiredEvent() {
+    while (!TimerQueue_.empty()) {
+        auto node = TimerQueue_.top();
+        if (node->isDelete())
+            TimerQueue_.pop();
+        else if (!node->isValid())
+            TimerQueue_.pop();
+        else
+            break;
+    }
+}
--- Timer.h
+++ Timer.h
@@ -1,5 +1,45 @@
+#pragma once
+#include <unistd.h>
+#include <deque>
+#include <memory>
+#include <queue>
+#include "base/Mutex.h"
+#include "base/noncopyable.h"
 
+class HttpData;
+//typedef std::shared_ptr<HttpData> SPHttpData;
+
+class TimerNode {
+ public:
+    typedef std::shared_ptr<HttpData> SPHttpData;
+    TimerNode(SPHttpData httpData, int timeout);
+    ~TimerNode();
+    TimerNode(TimerNode &tn);
+    void update(int timeout);
+    void clearReq();
+    void setDelete() { delete_ = true; }
+    bool isDelete() const { return delete_; }
+    size_t getExpiredTime() const { return expiredTime_; }
+    bool isValid();
+
+ private:
+    SPHttpData httpData_;
+    bool delete_;
+    size_t expiredTime_;
+};
+
+struct TimerComp {
+    bool operator()(TimerNode &t1, TimerNode &t2) {
+        return t1.getExpiredTime() > t2.getExpiredTime();
+    }
+};
 
 class TimerManager {
-
+ public:
+    typedef std::shared_ptr<HttpData> SPHttpData;
+    void add_timer(SPHttpData data, int timeout);
+    void handleExpiredEvent();
+ private:
+    typedef std::shared_ptr<TimerNode> SPTimerNode;
+    std::priority_queue<SPTimerNode, std::vector<SPTimerNode>, TimerComp> TimerQueue_;
 };
